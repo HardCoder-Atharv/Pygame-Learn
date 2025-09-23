bc3ce9d00a85389ae691be714c8e78293c9a79b2
HardCoder-Atharv <atharv.iitd@gmail.com>
2025-09-24 00:41:36 +0530

    new character introduced,it is the final one,gun might change,ramps have been removed

--- GameJam/gun_controller.py
+++ GameJam/gun_controller.py
@@ -11,17 +11,23 @@ gun_img = pygame.transform.scale(gun_img,(100,100))
 
 
 running = True
+
 while running:
     screen.fill((123,45,200))
-
+    angle_degrees = 0
     mx,my = pygame.mouse.get_pos()
-    if mx != gun_img_rect.x:
+    if mx - gun_img_rect.x > 0:
     
         angle_radian = math.atan((-my+gun_img_rect.y)/(mx-gun_img_rect.x))
+    if mx - gun_img_rect.x < 0:
+    
+        angle_radian = 3.14- math.atan((+my-gun_img_rect.y)/(mx-gun_img_rect.x)) 
+        
     angle_degrees = math.degrees(angle_radian)
-
+    pygame.draw.line(screen,(123,123,234),(0,gun_img_rect.y),(700,gun_img_rect.y))
+    pygame.draw.line(screen,(123,123,234),(gun_img_rect.x,0),(gun_img_rect.x,700))
     gun_rotate_img = pygame.transform.rotate(gun_img,angle_degrees)
-    screen.blit(gun_rotate_img,(300-gun_img.get_width()/2,300 ))
+    screen.blit(gun_rotate_img,(gun_img_rect.x-(gun_img.get_width())/2,gun_img_rect.y -(gun_img.get_height())/2))
 
 
     for event in pygame.event.get():
